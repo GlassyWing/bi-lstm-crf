ed72b6be0068052a5a0fb117fa3c778cf2b671f3
manlier <1490215053@qq.com>
2019-06-20 20:33:48 +0800

    update example

--- examples/decode_example.py
+++ examples/decode_example.py
@@ -1,12 +1,14 @@
 import time
-
+import logging
 from dl_segmenter import get_or_create, DLSegmenter
 
 if __name__ == '__main__':
+    LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+    logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
     segmenter: DLSegmenter = get_or_create("../data/default-config.json",
                                            src_dict_path="../data/src_dict.json",
                                            tgt_dict_path="../data/tgt_dict.json",
-                                           weights_path="../models/weights.01-0.15.h5")
+                                           weights_path="../models/weights.32--0.18.h5")
 
     texts = [
         "昨晚，英国首相特里萨•梅(TheresaMay)试图挽救其退欧协议的努力，在布鲁塞尔遭遇了严重麻烦。"
